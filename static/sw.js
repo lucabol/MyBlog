36b9c98a6a2e0fbf800fbfa8935115f11ef18dbe
Luca Bolognese <lucabol@microsoft.com>
2020-03-30 17:17:48 +0200

    Bring fontface into the css

--- static/sw.js
+++ static/sw.js
@@ -31,16 +31,18 @@ self.addEventListener('install', evt =>
 
 // https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#stale-while-revalidate
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.open(CURRENT_CACHE).then(function(cache) {
-      return cache.match(event.request).then(function(response) {
-        var fetchPromise = fetch(event.request).then(function(networkResponse) {
-          cache.put(event.request, networkResponse.clone());
-          return networkResponse;
+  if(event.request.method === "GET") {
+    event.respondWith(
+      caches.open(CURRENT_CACHE).then(function(cache) {
+        return cache.match(event.request).then(function(response) {
+          var fetchPromise = fetch(event.request).then(function(networkResponse) {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          })
+          return response || fetchPromise;
         })
-        return response || fetchPromise;
       })
-    })
-  );
+    );
+  }
 });
 
