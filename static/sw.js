0144e385d901d018ff2ea532bde4ff3046b3797d
Luca Bolognese <lucabol@microsoft.com>
2020-03-31 09:18:33 +0200

    Just cache fonts

--- static/sw.js
+++ static/sw.js
@@ -1,9 +1,12 @@
 // the cache version gets updated every time there is a new deployment
-const CACHE_VERSION = 10;
+const CACHE_VERSION = 11;
 const CURRENT_CACHE = `v${CACHE_VERSION}`;
 
 // these are the routes we are going to cache for offline support
 const cacheFiles = ['/', '/index.html', '/offline/', '/offline/index.html'];
+
+// these are the file type to cache with stale while revalidate
+const staleTypes = [/^font\//i]
 
 // on activation we clean up the previously registered service workers
 self.addEventListener('activate', evt =>
@@ -36,10 +39,25 @@ self.addEventListener('fetch', function(event) {
       caches.open(CURRENT_CACHE).then(function(cache) {
         return cache.match(event.request).then(function(response) {
           var fetchPromise = fetch(event.request).then(function(networkResponse) {
-            cache.put(event.request, networkResponse.clone());
+            console.log(`cached: ${networkResponse.headers.get('content-type')}`)
+            // Just add to the cache the stale types
+            if(networkResponse.status < 400 && networkResponse.headers.has('content-type') && networkResponse.headers.get('content-type').match(staleTypes)) {
+              cache.put(event.request, networkResponse.clone());
+            }
             return networkResponse;
           })
           return response || fetchPromise;
+        })
+        .catch(function(networkResponse) {
+            console.log(`network: ${networkResponse.headers.get('content-type')}`)
+          return fetch(event.request).then(function(networkResponse) {
+            // Just add to the cache the stale types
+            if(networkResponse.status < 400 && networkResponse.headers.has('content-type') && networkResponse.headers.get('content-type').match(staleTypes)) {
+              cache.put(event.request, networkResponse.clone());
+            }
+            return networkResponse;
+          })
+
         })
       })
     );
