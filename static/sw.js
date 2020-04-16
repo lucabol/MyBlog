88e91b7e652feab7d25035fe1ffbda85420d41b8
Luca Bolognese <lucabol@microsoft.com>
2020-04-16 08:28:43 +0200

    Use open source fonts (#1)
    * Bring fontface into the css
    
    * Add cardo
    
    * Just cache fonts
    
    * Change again service broker
    
    * Moving to file based menu
    
    * Change default to times new roman
    
    * Adding more styles
    
    * Adding CM fonts and change font menu
    
    * Making sorting by description work
    
    * Dynamic typography
    
    * Add other styles
    
    * Test shortcut
    
    * Changing look again
    
    * Move theme on top menu
    
    * Removed hugo-classic folder
    
    * Make code same as text
    
    * Add lucida files
    
    * Add backup for lucida
    
    * Add default to Lucida
    
    * Adding PTfonts
    
    * Aggiunto source
    
    * Add Antique Olive
    
    * Added FiraSans
    
    * Add sourcesans
    
    * Add Nimbus
    
    * Added URWClassico
    
    * Added Munson
    
    * changing navigation
    
    * Adding menus
    
    * Subsetted fonts, still to change css
    
    * Converting all css files
    
    * Changed weight source serif pro

--- static/sw.js
+++ static/sw.js
@@ -1,9 +1,12 @@
 // the cache version gets updated every time there is a new deployment
-const CACHE_VERSION = 10;
+const CACHE_VERSION = 11;
 const CURRENT_CACHE = `v${CACHE_VERSION}`;
 
 // these are the routes we are going to cache for offline support
 const cacheFiles = ['/', '/index.html', '/offline/', '/offline/index.html'];
+
+// these are the file type to cache with stale while revalidate
+const staleTypes = ['.woff', 'woff2', 'ttf']
 
 // on activation we clean up the previously registered service workers
 self.addEventListener('activate', evt =>
@@ -30,17 +33,28 @@ self.addEventListener('install', evt =>
 );
 
 // https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#stale-while-revalidate
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.open(CURRENT_CACHE).then(function(cache) {
-      return cache.match(event.request).then(function(response) {
-        var fetchPromise = fetch(event.request).then(function(networkResponse) {
-          cache.put(event.request, networkResponse.clone());
-          return networkResponse;
+self.addEventListener('fetch', event => {
+  if(event.request.method === 'GET' ) 
+    if(staleTypes.some(v => event.request.url.includes(v))) {
+      // For fonts returns it from cache and asynchronously download the latest one in case it changed
+      event.respondWith(caches.open(CURRENT_CACHE).then(cache => cache.match(event.request).then(response => {
+        const fetchPromise = fetch(event.request).then(networkResponse => {
+          if(networkResponse.status < 400 && event.request.url.indexOf('http') === 0) cache.put(event.request, networkResponse.clone())
+          return networkResponse
         })
-        return response || fetchPromise;
-      })
-    })
-  );
-});
-
+        return response || fetchPromise
+      })))
+    } else {
+      // For all the rest return it from the network, cache it and, if network error from cache
+      event.respondWith(fetch(event.request).then(networkResponse => {
+        const clonedResponse = networkResponse.clone()
+        caches.open(CURRENT_CACHE).then(cache => {
+          if(!(event.request.url.indexOf('http') === 0)){
+            return;
+          }
+          cache.put(event.request, clonedResponse)
+        })
+        return networkResponse;
+      }).catch(e => caches.open(CURRENT_CACHE).then(cache => cache.match(event.request))))
+    }
+})
