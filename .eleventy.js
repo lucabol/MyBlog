3f4dcf156c9bf73c1b0021656c12ac645943d900
Luca Bolognese <lucabol@microsoft.com>
2020-12-21 09:14:38 +0100

    Trying fixing svg fallback

--- .eleventy.js
+++ .eleventy.js
@@ -96,7 +96,7 @@ const inlineFunc = async (content, outputPath) => {
     if (!String(outputPath).endsWith('.html')) return content
 
     return await inlineSource(content, {
-        compress: false, /* comressing images breaks adding a fallback image inside the svg as discussed in https://css-tricks.com/a-complete-guide-to-svg-fallbacks/*/
+        compress: false, /* comressing images breaks adding a fallback image inside the svg as discussed in https://css-tricks.com/a-complete-guide-to-svg-fallbacks/ */
         handlers: [favicon] /* inlines favicon */
     })
 }
